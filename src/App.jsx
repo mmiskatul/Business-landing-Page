3e43ccdd10a353aac29c69fe0f16bc7504ba225b
mmiskatul <masabimiskat@gmail.com>
2025-05-04 17:57:15 +0600

    added gradient

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import Navbar from './component/Navbar'
 import Hero from './component/Hero'
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    </>
+    <main className='relative min-h-screen overflow-x-hidden'>
+      {/* Top-left background blur */}
+      <div className='absolute -top-28 -left-28 w-[800px] h-[800px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
+      
+      {/* Bottom-right optional blur */}
+      <div className='absolute -bottom-28 -right-28 w-[800px] h-[800px] bg-gradient-to-tr from-pink-500/20 to-indigo-500/20 rounded-full blur-[80px] -z-10'></div>
+      
+      <div className='overflow-hidden'>
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
   )
 }
 
